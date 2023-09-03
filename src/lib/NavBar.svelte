bfcbdcc46e7efe2e4c33770c5a337cd2cfcda9d2
ShowierData9978 <mellfang36@gmail.com>
2023-09-03 22:56:05 +0000

    Start commit

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,10 +1,82 @@
-<script lang="ts">
-
+<script>
+    import { goto } from "$app/navigation";
+    import { redirect } from "@sveltejs/kit";
 
 </script>
 
+<style>
+    .navbar {
+        background-color: #333;
+        overflow: hidden;
+        top: 0;
+        width: 100%;
+        margin-bottom: 12px;
+        
+    }
 
-<div>
-
+    .navbar-links {
     
+        /* make the links inline */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+    }
+
+    .navbar-item {
+        display: inline-block;
+        color: #f2f2f2;
+        display: block;
+        text-align: center;
+        text-decoration: none;
+        margin: 1rem;
+
+
+        /* make the buttons look like links */
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        outline: none;
+
+        font-size: medium;
+
+    }
+
+    .navbar-logo {
+        /* same as navbar-item. but align left */
+        display: inline-block;
+        color: #f2f2f2;
+        display: block;
+        text-align: left;
+        text-decoration: none;
+        margin: 1rem;
+        float: left;
+
+        /* make the buttons look like links */
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        outline: none;
+
+        font-size: large;
+    }
+
+    a {
+        color: white;
+        text-decoration: none;
+    }
+    </style>
+
+<div class="navbar">
+    
+    <button class="navbar-logo"> 
+        <a href="https://github.com/showierdata9978/"> 
+            ShowierData9978 
+        </a> 
+    </button>
+
+    <div class="navbar-links">
+        <button class="navbar-item" on:click={() => goto("/")}>Home</button>
+        <button class="navbar-item" on:click={() => goto("/about")}>About</button>
+    </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,75 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script lang="ts">
+    import NavBar from "$lib/NavBar.svelte";
+
+</script>
+
+<style>
+
+
+  img {
+    float: left;
+    margin: 5px;
+  }
+
+  p {
+    text-align: justify;
+    font-size: 25px;
+    padding-right: 24px;
+    padding-left: 2px;
+  }
+
+  .text-below-img {
+    
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+
+
+  }
+
+  .text-below-img p {
+      text-align: default;
+
+  }
+  
+
+  h1 {
+    text-align: justify;
+  }
+
+  a {
+    color: white;
+  }
+</style>
+<NavBar />
+
+
+<h2> 
+    
+    Hi, Im ShowierData9978
+</h2>
+
+<img src="https://avatars.githubusercontent.com/u/68120127?v=4" width="100" height="100" alt="ShowierData9978's PFP">
+<p>
+    Im a developer that works with alot of diffrent languges
+    because i can translate them to most other languges
+    specificly (the ones i use the most are) 
+    Python, Js, and Java. 
+</p>    
+
+<br><br><br>
+
+<p> 
+  I work on alot of projects, but the most notable one is <a href="https://meower.org/">Meower</a>!
+</p>
+
+
+<br>
+<div class="text-below-img">
+<img style="" src="https://camo.githubusercontent.com/d994e4dccc2e603be23b9689b45428463aa171903ee60acb81c75bc83c4a4214/68747470733a2f2f6d656f7765722e6f72672f6173736574732f6c6f676f2e737667" alt="Meower Media Co's logo">
+
+<p>
+Meower is a free, open-source, and non-profit social media platform. Meower is also a (currently WIP) alternative to Twitter, Discord, and Reddit,   completely free of advertising, tracking, data collection, and more! <br><br>The website and launch is available at https://meower.org<br><br>Go to https://github.com/meower-media-co to see the source code
+</p>
+</div>
