d176de66ccb8eeafc9543e63bea87ee596ebd7ce
ShowierData9978 <mellfang36@gmail.com>
2023-10-22 00:06:31 -0500

    Update +page.svelte

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,6 +1,6 @@
 <h1> I use </h1>
 <a href="https://www.abuseipdb.com/user/125934" title="AbuseIPDB is an IP address blacklist for webmasters and sysadmins to report IP addresses engaging in abusive behavior on their networks">
-	<img src="https://www.abuseipdb.com/contributor/125934.svg" alt="AbuseIPDB Contributor Badge" style="width: 401px;">
+	<img src="https://www.abuseipdb.com/contributor/125934.svg" alt="AbuseIPDB Contributor Badge" style="width: 791px;background: #35c246 linear-gradient(rgba(255,255,255,0), rgba(255,255,255,.3) 50%, rgba(0,0,0,.2) 51%, rgba(0,0,0,0));padding: 5px;">
 </a>
-
+<br>
 so beware of messing with my website or be banned
