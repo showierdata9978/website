1006cbe72ac020e3a8d4d589f21e409f8606dcd8
ShowierData9978 <mellfang36@gmail.com>
2023-10-28 15:58:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const logger = createLogger({
     myFormat
   ),
   transports: [
-      new transports.Console()
+      new transports.Console(),
       new transports.File({ filename: 'error.log', level: 'error' }),
       new transports.File({ filename: 'combined.log' }),
   ],
